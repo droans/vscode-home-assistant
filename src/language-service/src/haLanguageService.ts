94ae261ee9c8910195ddaa779e8c930a5e34b052
Michael Carroll <mjoecarroll@gmail.com>
2024-09-06 11:34:39 -0400

    Misc

--- src/language-service/src/haLanguageService.ts
+++ src/language-service/src/haLanguageService.ts
@@ -12,7 +12,7 @@ import {
   TextDocument,
   TextDocumentChangeEvent,
   TextEdit,
-  } from "vscode-languageserver-protocol";
+} from "vscode-languageserver-protocol";
 import { getLineOffsets } from "yaml-language-server/out/server/src/languageservice/utils/arrUtils";
 import {
   LanguageService,
